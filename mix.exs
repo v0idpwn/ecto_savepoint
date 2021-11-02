002f89459826ac18343d7765c2974e4ded82e028
v0idpwn <v0idpwn@gmail.com>
2021-11-02 13:25:35 +0200

    prepare for release on hex

--- mix.exs
+++ mix.exs
@@ -5,9 +5,18 @@ defmodule EctoSavepoint.MixProject do
     [
       app: :ecto_savepoint,
       version: "0.1.0",
-      elixir: "~> 1.13-dev",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Savepoint wrapper for Ecto with Postgrex/Myxql",
+      package: [
+        name: "ecto_savepoint",
+        licenses: ["Apache-2.0"],
+        links: %{"GitHub" => "https://github.com/v0idpwn/ecto_savepoint"}
+      ],
+      name: "EctoSavepoint",
+      source_url: "https://github.com/v0idpwn/ecto_savepoint",
+      homepage_url: "https://github.com/v0idpwn/ecto_savepoint"
     ]
   end
 
@@ -21,8 +30,7 @@ defmodule EctoSavepoint.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.25", only: :docs}
     ]
   end
 end
