6f2c5e2efdb0adaa7abd850deb5f028f4ba1a1d7
v0idpwn <v0idpwn@gmail.com>
2021-11-02 13:25:21 +0200

    implement basic functionality

--- lib/ecto_savepoint.ex
+++ lib/ecto_savepoint.ex
@@ -1,18 +1,36 @@
 defmodule EctoSavepoint do
   @moduledoc """
-  Documentation for `EctoSavepoint`.
+  Simple Ecto extension to use savepoints. Works with both postgrex and myxql.
+
+  Add this line in your Repo:
+      use EctoSavepoint
+
+  And you can call:
+      MyRepo.transaction(fn ->
+        Repo.insert(...)
+
+        Repo.savepoint("my_savepoint")
+
+        Repo.insert(...)
+
+        Repo.rollback_to_savepoint("my_savepoint")
+
+        Repo.insert(...)
+      end)
   """
 
-  @doc """
-  Hello world.
+  def savepoint(repo, name) do
+    repo.query("SAVEPOINT #{name}")
+  end
 
-  ## Examples
+  def rollback_to_savepoint(repo, name) do
+    repo.query("ROLLBACK TO SAVEPOINT #{name}")
+  end
 
-      iex> EctoSavepoint.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defmacro __using__(_opts) do
+    quote do
+      def savepoint(name), do: EctoSavepoint.savepoint(get_dynamic_repo(), name)
+      def rollback_to_savepoint(name), do: EctoSavepoint.rollback_to_savepoint(get_dynamic_repo(), name)
+    end
   end
 end
--- test/ecto_savepoint_test.exs
+++ test/ecto_savepoint_test.exs
@@ -2,7 +2,20 @@ defmodule EctoSavepointTest do
   use ExUnit.Case
   doctest EctoSavepoint
 
-  test "greets the world" do
-    assert EctoSavepoint.hello() == :world
+  defmodule FakeRepo do
+    use EctoSavepoint
+
+    def get_dynamic_repo, do: __MODULE__
+    def query(string), do: string
+  end
+
+  test "the functions call the proper queries" do
+    assert "SAVEPOINT my_savepoint" == EctoSavepoint.savepoint(FakeRepo, "my_savepoint")
+    assert "ROLLBACK TO SAVEPOINT my_savepoint" == EctoSavepoint.rollback_to_savepoint(FakeRepo, "my_savepoint")
+  end
+
+  test "injects the functions" do
+    assert function_exported?(FakeRepo, :savepoint, 1)
+    assert function_exported?(FakeRepo, :rollback_to_savepoint, 1)
   end
 end
