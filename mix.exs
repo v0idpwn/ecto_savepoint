d365afd6fe676b79f6055b828565f22c55e0993b
v0idpwn <v0idpwn@gmail.com>
2021-11-02 13:29:01 +0200

    set runtime: false for ex_doc

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule EctoSavepoint.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.25", only: :docs}
+      {:ex_doc, "~> 0.25", only: :docs, runtime: false}
     ]
   end
 end
